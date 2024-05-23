195e1510d804d66074bd1cd90f2391338037108c
Vanamali Sims <isvanamali@gmail.com>
2024-05-23 22:37:02 +0530

    view work fix

--- portfolio/src/components/Home.jsx
+++ portfolio/src/components/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { HiArrowNarrowRight } from 'react-icons/hi'
+import { Link } from 'react-scroll';
 
 const Home = () => {
   return (
@@ -12,12 +13,14 @@ const Home = () => {
         <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>I'm a Software Developer.</h2>
         <p className='text-[#8892b0] py-4 max-w-[700px]'>I am a versatile full stack developer and machine learning engineer, skilled in both front-end and back-end technologies. With a strong foundation in machine learning</p>
         <div>
+        <Link to='work' smooth={true} duration={500}>
           <button className='text-white group: border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600 duration-300'>
           View Work
           <span className='group-hover:rotate-90 duration-300'>
             <HiArrowNarrowRight className='ml-3'/>
           </span>
             </button>
+            </Link>
         </div>
       </div>
     </div>
